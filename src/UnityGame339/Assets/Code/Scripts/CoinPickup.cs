711ea67d4a93052efa32c6e18293b3c86486e895
Jake Milam <jmilam@chapman.edu>
2026-04-07 18:36:18 -0700

    Coins create floating text

--- src/UnityGame339/Assets/Code/Scripts/CoinPickup.cs
+++ src/UnityGame339/Assets/Code/Scripts/CoinPickup.cs
@@ -1,13 +1,20 @@
 using UnityEngine;
 
-public class CoinPickup : MonoBehaviour
+public class CoinPickup : DeathEffectObject
 {
     [SerializeField] private int currencyWorth = 100;
     [SerializeField] private AudioClip pickupSound;
+    
+    public GameObject FloatingScorePrefab;
 
     private void OnMouseDown()
     {
         LevelManager.main.IncreaseCurrency(currencyWorth);
+        
+        // floating score text
+        FloatingText floatingText = FloatingScorePrefab.GetComponent<FloatingText>();
+        floatingText.SetText(currencyWorth);
+        Instantiate(FloatingScorePrefab, transform.position, Quaternion.identity);
 
         // sound
         if (pickupSound != null)
