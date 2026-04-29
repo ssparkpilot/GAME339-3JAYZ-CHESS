39757b71c264f3a701b6bfcc55226f7f8103e471
Jude-Abatangelo <abatangelo@chapman.edu>
2026-04-29 01:32:06 -0700

    Fixed small stuff

--- src/UnityGame339/Assets/Code/Scripts/Chess/EnemyView.cs
+++ src/UnityGame339/Assets/Code/Scripts/Chess/EnemyView.cs
@@ -59,18 +59,18 @@ public class EnemyView : MonoBehaviour
     }
 
     private void OnMouseEnter()
-{
-    if (HoverHealthUI.main == null) return;
-    if (unit == null) return;
-
-    HoverHealthUI.main.Show(transform.position, unit.Health);
-}
-
-private void OnMouseExit()
-{
-    if (HoverHealthUI.main != null)
     {
-        HoverHealthUI.main.Hide();
+        if (HoverHealthUI.main == null) return;
+        if (unit == null) return;
+
+        HoverHealthUI.main.Show(transform.position, unit.Health);
     }
-}
+
+    private void OnMouseExit()
+    {
+        if (HoverHealthUI.main != null)
+        {
+            HoverHealthUI.main.Hide();
+        }
+    }
 }
