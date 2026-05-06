8ff60b319ec0822cbb335af812f43fd15914b5b8
Jake Milam <jmilam@chapman.edu>
2026-05-06 01:31:54 -0700

    Knight now only moves right and now all pieces move starting rightward

--- src/UnityGame339/Assets/Code/Scripts/Chess/EnemyMovementController.cs
+++ src/UnityGame339/Assets/Code/Scripts/Chess/EnemyMovementController.cs
@@ -6,7 +6,7 @@ using Game339.Shared.Services;
 
 public class EnemyMovementController : MonoBehaviour
 {
-    [SerializeField] private float moveInterval = 5f;
+    [SerializeField] private float moveInterval = 2f;
     [SerializeField] private float delayBetweenEachEnemyMove = 0.2f;
 
     private EnemyMovementService movementService;
@@ -27,8 +27,12 @@ public class EnemyMovementController : MonoBehaviour
         {
             yield return new WaitForSeconds(moveInterval);
 
+            // moves rightmost enemies first
             var board = BoardManager.main.Board;
-            var enemies = board.Enemies.ToList();
+            var enemies = BoardManager.main.Board.Enemies
+                .OrderByDescending(e => e.Position.X)
+                .ThenByDescending(e => e.Position.Y)
+                .ToList();
 
             foreach (var enemy in enemies)
             {
