376c379aaa04129cf48a4e0467f71477873e385f
Jake Milam <jmilam@chapman.edu>
2026-05-12 17:32:44 -0700

    removed need for unity for a random range

--- src/Shared/Game339.Shared/Runtime/Services/Implementation/EnemyMovementService.cs
+++ src/Shared/Game339.Shared/Runtime/Services/Implementation/EnemyMovementService.cs
@@ -1,11 +1,13 @@
+using System;
 using System.Linq;
 using Game339.Shared.Models;
 using Game339.Shared.Services;
 using Game339.Shared.Services.Implementation;
-using UnityEngine;
 
 public class EnemyMovementService
 {
+    private static readonly Random rng = new Random();
+    
     public void ExecuteEnemyMove(EnemyUnit enemy, GridBoard board)
     {
         if (enemy == null || board == null)
@@ -16,7 +18,7 @@ public class EnemyMovementService
         if (legalMoves.Count == 0)
             return;
 
-        var moveTo = legalMoves[Random.Range(0, legalMoves.Count)];
+        var moveTo = legalMoves[rng.Next(legalMoves.Count)]; // random runs outside of unity
         board.MoveUnit(enemy, moveTo);
     }
 }
